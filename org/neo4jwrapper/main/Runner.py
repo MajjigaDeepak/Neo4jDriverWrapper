e3d01cbeedde992a2f2ae16b1dc9c3e7a7294a6e
nishantgandhi99 <nishantgandhi99@gmail.com>
2016-09-14 00:27:47 +0530

    query return result in JSON

--- org/neo4jwrapper/main/Runner.py
+++ org/neo4jwrapper/main/Runner.py
@@ -1,4 +1,4 @@
-from neo4j.v1 import GraphDatabase, basic_auth
+from neo4j.v1 import GraphDatabase, basic_auth, Node,Relationship, Path
 
 class Neo4jWrapper:
     driver = None
@@ -11,17 +11,43 @@ class Neo4jWrapper:
         self.driver = GraphDatabase.driver("bolt://"+self.host, auth=basic_auth(self.user, self.password))
         print "Driver Initialized successfully with IP:"+self.host+" & User:" + self.user
 
-    # return Graph in the form of JSON
+    # TODO: Path should be converted in JSON when PATH is returned in query
+    # return Graph in the form of JSON. The query can return only sub-graph or sub-graph & properties
+    # result has list of Nodes, Relationships and Path with all details
     def graphTraversalQuery(self, query):
         self.session = self.driver.session()
         result = self.session.run(query)
+        node = []
+        relationship = []
+        path = []
         for record in result:
             for key in record.keys():
-                print key + record[key]
+                if isinstance(record[key],Node):
+                    n = dict()
+                    n['id'] = record[key].id
+                    n['label'] = list(record[key].labels)
+                    n['properties'] = record[key].properties
+                    node.append(n)
+                if isinstance(record[key], Relationship):
+                    r = dict()
+                    r['id'] = record[key].id
+                    r['start'] = record[key].start
+                    r['end'] = record[key].end
+                    r['type'] = record[key].type
+                    r['properties'] = record[key].properties
+                    relationship.append(r)
+                if isinstance(record[key], Path):
+                    path.append(record[key])
+        graph = dict()
+        graph['nodes'] = node
+        graph['relationship'] = relationship
+        graph['path'] = path
         self.session.close()
+        return graph
 
     # return Property of node/relationship in list of JSON.
     # Only pass the query that return property and not sub-Graph
+    # the result is something similar to Table structure
     def graphPropertyQuery(self, query):
         self.session = self.driver.session()
         result = self.session.run(query)
@@ -31,12 +57,10 @@ class Neo4jWrapper:
             for key in record.keys():
                 mapping[key] = record[key]
             list.append(mapping)
-        print list
         self.session.close()
         return list
 
 if __name__ == "__main__":
     wrapper = Neo4jWrapper()
-  #  wrapper.graphTraversalQuery("match (n) return n Limit 5")
-    wrapper.graphPropertyQuery("match (n) return n.id,n.name Limit 5")
-
+    print wrapper.graphTraversalQuery("match (n:START_UP{name:\"Meesho\"})-[r:GOT_INVESTMENT]-()-[p]-() return n,r,p")
+    print wrapper.graphPropertyQuery("MATCH (n) RETURN ID(n) AS id,n.name AS name Limit 5")
